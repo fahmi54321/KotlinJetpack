b6f454b5ccab42cb454251d2ca82029ee74414dd
fahmi <fahmiabdulaziz44@gmail.com>
2023-12-24 20:45:53 +0700

    data binding

--- app/src/main/java/com/example/kotlinjetpack/MainActivity.kt
+++ app/src/main/java/com/example/kotlinjetpack/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.kotlinjetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.kotlinjetpack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // todo 3
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // todo 4
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        // todo 5 (finish)
+        binding.btnSimpan.setOnClickListener {
+            var data = binding.edtName.text.toString()
+            binding.txtHelloWorld.text = data
+        }
     }
 }
